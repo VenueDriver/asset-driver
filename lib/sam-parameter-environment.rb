9c6b7ffc1f69f9a21a5be0b93790a3cdd74fa4f7
Ryan Alyn Porter <rap@endymion.com>
2021-01-05 20:32:11 -0500

    Bug: Don’t load the environment variables in the cloud!
    This code that was intended for use in local development and testing was running in AWS Lambda, and the code ended up thinking that it was in the ‘dev’ environment.  It was very confusing to understand why it kept trying to connect to the development S3 bucket.  TODO: The bug couldn’t have happened if the samconfig.toml file hadn’t been built into the package for Lambda.  It would be better if only the code needed for runtime were included in the package.

--- lib/sam-parameter-environment.rb
+++ lib/sam-parameter-environment.rb
@@ -7,6 +7,15 @@ module SamParameterEnvironment
 # 'parameter_overrides' setting in the SAM configuration file instead of from
 # a .env file.
   def self.load(environment=nil)
+    # This code that was intended for use in local development and testing was
+    # running in AWS Lambda, and the code ended up thinking that it was in the
+    # ‘dev’ environment.  It was very confusing to understand why it kept trying
+    # to connect to the development S3 bucket.
+    # TODO: The bug couldn’t have happened if the samconfig.toml file hadn’t
+    # been built into the package for Lambda.  It would be better if only the
+    # code needed for runtime were included in the package.
+    return if ENV['AWS_EXECUTION_ENV']
+
     return unless File.exist? 'samconfig.toml'
     environment = ARGV[1] || 'default'
     TOML.load_file('samconfig.toml')[environment]['deploy']['parameters'
